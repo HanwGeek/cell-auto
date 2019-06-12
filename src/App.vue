b7ebda7492060f48fb081315afaad8d240706823
HanwGeek <HanwGeek@gmail.com>
2019-06-12 13:41:14 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    Hello
+    <CellAuto />
+  </div>
+</template>
+
+<script>
+import CellAuto from './components/cell-auto'
+export default {
+  name: 'App',
+  components: {
+    CellAuto
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/cell-auto.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="cell-auto">
+    <el-row>
+       <el-card style="width: 1000px;height: 600px">
+      <canvas 
+      id="cvs"
+      ref="cvs">
+      </canvas>
+       </el-card>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CellAuto",
+  data () {
+    return {
+      cvs: null
+    }
+  },
+  mounted () {
+    this.cvs = this.$refs.cvs;
+    this.cxt = this.cvs.getContext("2d");
+  },
+  methods: {
+
+  }
+}
+</script>
