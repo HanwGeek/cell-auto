1d55d250038ba42827baca266e9edc219ef501e4
yating <1064786402@qq.com>
2019-06-13 12:47:50 +0800

    create Global.vue file to restore constant

---
+++ src/Global.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+<script type="text/javascript">
+    const GRAY = "#CCC"
+    const ALIVE = 1
+    const DEAD = 0
+
+    export default
+    {
+        GRAY,
+        ALIVE,
+        DEAD
+    }
+</script>
+<style>
+</style>
